bcf71e8c804697209ed89dbe383ff14d7ad87ef6
Tomas Odehnal <tomasodehnal@localhost.localdomain>
2021-02-24 22:11:42 +0100

    Added Bootstrap 4.0 and Vue.js

--- source/routes/web.php
+++ source/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['name' => 'Kokos']);
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
