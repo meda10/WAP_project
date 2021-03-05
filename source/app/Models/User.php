7d67795a43ca5f57a056d07904664b7204f2383b
Roman Bártl <bbartl.roman@gmail.com>
2021-03-05 10:39:41 +0100

    Gate permission prepared

--- source/app/Models/User.php
+++ source/app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,30 @@ class User extends Authenticatable
     /*protected $casts = [
         'email_verified_at' => 'datetime',
     ];*/
+
+
+    public function hasRole($roles) 
+    {
+        foreach ($roles as $role)
+            if ($this->role == $role) return true;
+        return false;
+    }
+
+
+    public function isConfirmed()
+    {
+        return boolval($this->confirmed);
+    }
+
+    
+    public static function getPossibleRoles()
+    {
+        $type = DB::select(DB::raw('SHOW COLUMNS FROM users WHERE Field = "role"'))[0]->Type;
+        preg_match('/^enum\((.*)\)$/', $type, $matches);
+        $values = array();
+        foreach(explode(',', $matches[1]) as $value){
+            $values[] = trim($value, "'");
+        }
+        return $values;
+    }
 }
--- source/app/Providers/AuthServiceProvider.php
+++ source/app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        //'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('reserve-title', function($user) {
+            return $user->hasRole(['manager', 'customer', 'owner', 'employee']) && $user->isConfirmed();
+        });
     }
 }
