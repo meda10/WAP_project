3e902b23005ff57bae51506e244a4ba6f15836f8
Roman Bártl <bbartl.roman@gmail.com>
2021-03-06 18:09:38 +0100

    Initialization of titles

--- source/app/Models/State.php
+++ source/app/Models/State.php
@@ -6,16 +6,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Helpers\AppHelper;
 
-
-class Title extends Model
+class State extends Model
 {
     use HasFactory;
-    
-    const UPDATED_AT = NULL;
+
+    public $timestamps = false;
 
     public static function create(array $attributes = [])
     {
-        $attributes['url'] = AppHelper::friendlyUrl($attributes['title_name']);
+        $attributes['icon_path'] = AppHelper::friendlyUrl($attributes['state_name']);
         return static::query()->create($attributes);
     }
 }
