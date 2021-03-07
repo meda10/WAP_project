c1566327f86f706a11f2db03a45d8939eed3f965
Tomas Odehnal <odehnal.tomas136@gmail.com>
2021-03-07 23:46:22 +0100

    Added show movie list and routing (without grid view).

--- source/resources/js/components/Movies.vue
+++ source/resources/js/components/Movies.vue
@@ -4,9 +4,13 @@
     <div class="row">
         <div class="col-10">
             <div class="form-group">
-                <label for="exampleSelect1">Výběr žánru:</label>
-                <select class="form-control" id="exampleSelect1">
-                    <option v-for="genre in moviesGenres">{{ genre.name }}</option>
+                <label for="genresSelect">Výběr žánru:</label>
+                <select class="form-control" id="genresSelect" @change="changeGenre">
+                    <option v-for="(ge, key) in moviesGenres"
+                            :selected="(genre.name === ge.name)"
+                            :key="key" v-bind:value="ge.url">
+                        {{ ge.name }}
+                    </option>
                 </select>
             </div>
         </div>
@@ -111,42 +115,19 @@
     <table class="table table-hover" v-if="!renderView.gridView && renderView.listView">
         <thead>
         <tr>
-            <th scope="col">Type</th>
-            <th scope="col">Column heading</th>
-            <th scope="col">Column heading</th>
-            <th scope="col">Column heading</th>
+            <th scope="col">No.</th>
+            <th scope="col">Název</th>
+            <th scope="col">Rok vydání</th>
+            <th scope="col">Popisek</th>
         </tr>
         </thead>
         <tbody>
-        <tr class="table-dark">
-            <th>Default</th>
-            <td>Column content</td>
-            <td>Column content</td>
-            <td>Column content</td>
-        </tr>
-        <tr>
-            <th>Default</th>
-            <td>Column content</td>
-            <td>Column content</td>
-            <td>Column content</td>
-        </tr>
-        <tr class="table-dark">
-            <th>Dark</th>
-            <td>Column content</td>
-            <td>Column content</td>
-            <td>Column content</td>
-        </tr>
-        <tr>
-            <th>Default</th>
-            <td>Column content</td>
-            <td>Column content</td>
-            <td>Column content</td>
-        </tr>
-        <tr class="table-dark">
-            <th>Dark</th>
-            <td>Column content</td>
-            <td>Column content</td>
-            <td>Column content</td>
+        <tr v-for="(title, key) in titles.list"
+            v-bind:class="!(key%2) ? 'table-dark' : ''">
+            <td>{{ key+1 }}</td>
+            <th>{{ title.title_name }}</th>
+            <td>{{ title.year }}</td>
+            <td>{{ title.description.substring(0, 300)+"..." }}</td>
         </tr>
         </tbody>
     </table>
@@ -155,26 +136,26 @@
     <div class="row justify-content-center mt-4">
         <ul class="pagination" v-if="titles.pageNumbers < 8">
             <li class="page-item"
-                @click="titles.pageNumber--"
+                @click="changePageNum(titles.pageNumber-1)"
                 v-bind:class="(titles.pageNumber <= 1) ? 'disabled' : ''">
                 <a class="page-link" href="#">&laquo;</a>
             </li>
             <!--      When number of pages <= 7      -->
             <li class="page-item"
                 v-for="num in titles.pageNumbers"
-                @click="titles.pageNumber = num"
+                @click="changePageNum(num)"
                 v-bind:class="(titles.pageNumber === num) ? 'active' : ''">
                 <a class="page-link" href="#">{{ num }}</a>
             </li>
             <li class="page-item"
-                @click="titles.pageNumber++"
+                @click="changePageNum(titles.pageNumber+1)"
                 v-bind:class="(titles.pageNumber >= titles.pageNumbers) ? 'disabled' : ''">
                 <a class="page-link" href="#">&raquo;</a>
             </li>
         </ul>
         <ul class="pagination" v-if="titles.pageNumbers >= 8">
             <li class="page-item"
-                @click="titles.pageNumber--"
+                @click="changePageNum(titles.pageNumber-1)"
                 v-bind:class="(titles.pageNumber <= 1) ? 'disabled' : ''">
                 <a class="page-link" href="#">&laquo;</a>
             </li>
@@ -182,7 +163,7 @@
             <!--      When number of pages       -->
             <li class="page-item"
                 v-for="num in 2"
-                @click="titles.pageNumber = num"
+                @click="changePageNum(num)"
                 v-bind:class="(titles.pageNumber === num) ? 'active' : ''">
                 <a class="page-link" href="#">{{ num }}</a>
             </li>
@@ -194,7 +175,7 @@
             <li class="page-item"
                 v-if="(titles.pageNumber <= 4)"
                 v-for="num in 3"
-                @click="titles.pageNumber = num+2"
+                @click="changePageNum(num+2)"
                 v-bind:class="(titles.pageNumber === (num+2)) ? 'active' : ''">
                 <a class="page-link" href="#">{{ num+2 }}</a>
             </li>
@@ -202,7 +183,7 @@
             <li class="page-item"
                 v-if="(titles.pageNumber > 4) && (titles.pageNumber < (titles.pageNumbers-3))"
                 v-for="num in 3"
-                @click="titles.pageNumber = titles.pageNumber-2+num"
+                @click="changePageNum(titles.pageNumber-2+num)"
                 v-bind:class="(titles.pageNumber === (titles.pageNumber-2+num)) ? 'active' : ''">
                 <a class="page-link" href="#">{{ (titles.pageNumber-2+num) }}</a>
             </li>
@@ -214,24 +195,24 @@
             <li class="page-item"
                 v-if="(titles.pageNumber >= (titles.pageNumbers-3))"
                 v-for="num in 3"
-                @click="titles.pageNumber = titles.pageNumbers-5+num"
+                @click="changePageNum(titles.pageNumbers-5+num)"
                 v-bind:class="(titles.pageNumber === (titles.pageNumbers-5+num)) ? 'active' : ''">
                 <a class="page-link" href="#">{{ (titles.pageNumbers-5+num) }}</a>
             </li>
 
             <li class="page-item"
-                @click="titles.pageNumber = titles.pageNumbers-1"
+                @click="changePageNum(titles.pageNumbers-1)"
                 v-bind:class="(titles.pageNumber === (titles.pageNumbers-1)) ? 'active' : ''">
                 <a class="page-link" href="#">{{ (titles.pageNumbers-1) }}</a>
             </li>
             <li class="page-item"
-                @click="titles.pageNumber = titles.pageNumbers"
+                @click="changePageNum(titles.pageNumbers)"
                 v-bind:class="(titles.pageNumber === (titles.pageNumbers)) ? 'active' : ''">
                 <a class="page-link" href="#">{{ (titles.pageNumbers) }}</a>
             </li>
 
             <li class="page-item"
-                @click="titles.pageNumber++"
+                @click="changePageNum(titles.pageNumber + 1)"
                 v-bind:class="(titles.pageNumber >= titles.pageNumbers) ? 'disabled' : ''">
                 <a class="page-link" href="#">&raquo;</a>
             </li>
@@ -263,7 +244,7 @@ export default {
                 ordering: 'asc',
                 pageNumber: 1,
                 titlesToPage : 2,
-                numberOfGenreTitles: 20,
+                numberOfGenreTitles: 0,
                 pageNumbers: 0
             }
         }
@@ -271,35 +252,38 @@ export default {
     watch: {
         $route (to, from) {
             this.getGenreByUrl();
-            this.getTitles();
-            this.countNumOfPages();
-        },
-        'titles.pageNumber': {
-            handler: function (newVal, oldVal) {
-                if (this.titles.pageNumber < 1 || this.titles.pageNumber > this.titles.pageNumbers) {
-                    this.titles.pageNumber = oldVal;
-                }
-            }
-        },
+            this.getTitles(to.params.movieGenre);
+        }
     },
     mounted() {
         this.getGenreByUrl();
         this.getGenres();
-        this.getTitles();
-        this.countNumOfPages();
+        this.getTitles(this.genre.url);
 
         if (this.$route.params.genre != null)
             this.genre.url = this.$route.params.genre;
     },
     methods: {
+        changePageNum(val) {
+            if (val >= 1 && val <= this.titles.pageNumbers && this.titles.pageNumber !== val) {
+                this.titles.pageNumber = val;
+                this.getTitles(this.genre.url);
+            }
+        },
+        changeGenre(val) {
+            this.titles.pageNumber = 1;
+            this.$router.push({path: '/filmy/' + val.target.value});
+        },
         countNumOfPages() {
             this.titles.pageNumbers = Math.ceil(this.titles.numberOfGenreTitles / this.titles.titlesToPage);
         },
-        getTitles() {
-            let request = {type: this.genre.name, genre_url: this.genre.url,
+        getTitles(url) {
+            let request = {type: 'movie', genre_url: url,
                 number_of_titles: this.titles.titlesToPage, page_number: this.titles.pageNumber, order: this.titles.ordering};
             axios.post('/api/get_titles', request).then((res) => {
-                console.log(res.data);
+                this.titles.list = res.data.titles;
+                this.titles.numberOfGenreTitles = res.data.titles_count;
+                this.countNumOfPages();
             }).catch((error) => {
                 console.log(error);
             });
@@ -319,7 +303,9 @@ export default {
 
             axios.post('/api/genre_info_from_url', {'url' : this.$route.params.movieGenre}).then((res) => {
                 this.genre.name = res.data.name;
-                // this.genre.url = res.data.url
+                this.genre.url = this.$route.params.movieGenre
+                console.log(this.genre.name);
+                console.log(this.genre.url);
                 this.$emit('emitIsLoading', false);
             }).catch((error) => {
                 // TODO handle this error
