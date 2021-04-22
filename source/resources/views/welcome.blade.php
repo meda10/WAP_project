92dbaf5c9f83b177b5d9d785d03881d5e3240771
meda10 <32466683+meda10@users.noreply.github.com>
2021-04-22 09:43:52 +0200

    blade fix

--- source/resources/views/welcome.blade.php
+++ source/resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
         <script type="text/javascript">
             window.Laravel = {
                 csrfToken: "{{ csrf_token() }}",
-                jsPermissions: {!! auth()->check()?auth()->user()->jsPermissions():null !!}
+                jsPermissions: {!! auth()->check()?auth()->user()->jsPermissions():'' !!}
             }
         </script>
     </head>
